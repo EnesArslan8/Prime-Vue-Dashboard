8f01b8b7d2ea16c9af4942b73796a73ac65453df
EnesArslan8 <enes8arslan@gmail.com>
2023-09-03 04:43:42 +0300

    Added dropdown a twice

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,9 +9,115 @@ import Accordion from "primevue/accordion";
 import AccordionTab from "primevue/accordiontab";
 import TabView from "primevue/tabview";
 import TabPanel from "primevue/tabpanel";
+import Splitter from "primevue/splitter";
+import SplitterPanel from "primevue/splitterpanel";
+import Card from "primevue/card";
+import Button from "primevue/button";
+import Dropdown from "primevue/dropdown";
 
 const visible = ref(false);
 const toggle = ref(false);
+const selectedCountry = ref("");
+const selectedCity=ref("")
+const countries = ref([
+  {
+    name: "Australia",
+    code: "AU",
+  },
+  {
+    name: "Brazil",
+    code: "BR",
+  },
+  {
+    name: "China",
+    code: "CH",
+  },
+  {
+    name: "Egypt",
+    code: "EG",
+  },
+  {
+    name: "France",
+    code: "FR",
+  },
+  {
+    name: "Germany",
+    code: "GM",
+    items: [
+      { label: "Berlin", value: "Berlin" },
+      { label: "Frankfurt", value: "Frankfurt" },
+      { label: "Hamburg", value: "Hamburg" },
+      { label: "Munich", value: "Munich" },
+    ],
+  },
+  {
+    name: "India",
+    code: "IN",
+  },
+  {
+    name: "Japan",
+    code: "JA",
+    items: [
+      { label: "Kyoto", value: "Kyoto" },
+      { label: "Osaka", value: "Osaka" },
+      { label: "Tokyo", value: "Tokyo" },
+      { label: "Yokohama", value: "Yokohama" },
+    ],
+  },
+  {
+    name: "Spain",
+    code: "SP",
+  },
+  {
+    name: "United States",
+    code: "US",
+    items: [
+      { label: "Chicago", value: "Chicago" },
+      { label: "Los Angeles", value: "Los Angeles" },
+      { label: "New York", value: "New York" },
+      { label: "San Francisco", value: "San Francisco" },
+    ],
+  },
+  {
+    name: "Turkiye",
+    code: "TU",
+  },
+]);
+const groupedCities = ref([
+  {
+    name: "Germany",
+    code: "GM",
+    items: [
+      { label: "Berlin", value: "Berlin" },
+      { label: "Frankfurt", value: "Frankfurt" },
+      { label: "Hamburg", value: "Hamburg" },
+      { label: "Munich", value: "Munich" },
+    ],
+  },
+  {
+    name: "Japan",
+    code: "JA",
+    items: [
+      { label: "Kyoto", value: "Kyoto" },
+      { label: "Osaka", value: "Osaka" },
+      { label: "Tokyo", value: "Tokyo" },
+      { label: "Yokohama", value: "Yokohama" },
+    ],
+  },
+  {
+    name: "United States",
+    code: "US",
+    items: [
+      { label: "Chicago", value: "Chicago" },
+      { label: "Los Angeles", value: "Los Angeles" },
+      { label: "New York", value: "New York" },
+      { label: "San Francisco", value: "San Francisco" },
+    ],
+  }
+]);
+const generateFlagImageUrl = (code) => {
+  return `https://www.worldometers.info/img/flags/${code.toLowerCase()}-flag.gif`;
+};
 </script>
 
 <template>
@@ -32,9 +138,9 @@ const toggle = ref(false);
               nisi ut aliquip ex ea commodo consequat.
             </p>
           </Sidebar>
-          <Button class="hamburgerBtn" @click="visible = true"
-            ><i class="pi pi-bars"></i
-          ></Button>
+          <button class="hamburgerBtn" @click="visible = true">
+            <i class="pi pi-bars"></i>
+          </button>
         </div>
       </div>
       <div class="headerRight">
@@ -100,7 +206,7 @@ const toggle = ref(false);
           <TabView>
             <TabPanel>
               <template #header>
-                <i style="margin-right: .5rem;" class="pi pi-wallet"></i>
+                <i style="margin-right: 0.5rem" class="pi pi-wallet"></i>
                 <span>Hisse Nasıl Alınır</span>
               </template>
               <p>
@@ -113,9 +219,9 @@ const toggle = ref(false);
                 sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </TabPanel>
-            <TabPanel >
+            <TabPanel>
               <template #header>
-                <i style="margin-right: .5rem;" class="pi pi-question"></i>
+                <i style="margin-right: 0.5rem" class="pi pi-question"></i>
                 <span>Halka Arz Nedir</span>
               </template>
               <p>
@@ -134,18 +240,158 @@ const toggle = ref(false);
       </div>
     </div>
     <div class="imgAdsSection">
-
+      <div class="imgAdsContainer">
+        <div class="splitter" style="margin-bottom: 5rem">
+          <Splitter style="height: 300px; background-color: #333" class="mb-5">
+            <SplitterPanel
+              class="flex align-items-center justify-content-center"
+            >
+              <img
+                style="
+                  width: 100%;
+                  max-width: 100%;
+                  height: 100%;
+                  padding: 0.5rem;
+                  border-radius: 20px;
+                  filter: drop-shadow(0 5px 20px rgba(16, 97, 219, 0.5));
+                "
+                src="https://imgrosetta.mynet.com.tr/file/17160661/17160661-728xauto.jpg"
+              />
+            </SplitterPanel>
+            <SplitterPanel
+              class="flex align-items-center justify-content-center"
+            >
+              <img
+                style="
+                  width: 100%;
+                  max-width: 100%;
+                  height: 100%;
+                  padding: 0.5rem;
+                  border-radius: 20px;
+                  filter: drop-shadow(0 5px 20px rgba(16, 97, 219, 0.5));
+                "
+                src="https://www.alnusyatirim.com/static/uploads/hisse-senedi-fiyatlari-2.jpeg"
+              />
+            </SplitterPanel>
+          </Splitter>
+          <div class="selectCountry" style="margin-top: 3rem; ">
+            <Dropdown
+              v-model="selectedCountry"
+              :options="countries"
+              optionLabel="name"
+              placeholder="Select a Country"
+              class="w-full md:w-14rem"
+              style="width: 50%"
+            >
+              <template #value="slotProps">
+                <div v-if="slotProps.value" class="flex align-items-center">
+                  <!-- <img
+                    :alt="slotProps.value.label"
+                    src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                    
+                  /> -->
+                  <div>{{ slotProps.value.name }}</div>
+                </div>
+                <span v-else>
+                  {{ slotProps.placeholder }}
+                </span>
+              </template>
+              <template #option="slotProps">
+                <div
+                  class="flex align-items-center"
+                  style="display: flex; width: 100%"
+                >
+                  <img
+                    :alt="slotProps.option.label"
+                    :src="generateFlagImageUrl(slotProps.option.code)"
+                    alt="Flag"
+                    style="width: 32px; margin-right: 1rem"
+                  />
+                  <div>{{ slotProps.option.name }}</div>
+                </div>
+              </template>
+            </Dropdown>
+            <Dropdown
+              v-model="selectedCity"
+              :options="groupedCities"
+              optionLabel="label"
+              optionGroupLabel="label"
+              optionGroupChildren="items"
+              placeholder="Mevcut Halka Arzlar"
+              class="w-full md:w-14rem"
+              style="width: 45%;margin-left: 5%;"
+            >
+              <template #optiongroup="slotProps">
+                <div
+                  v-if="slotProps.option.items"
+                  class="flex align-items-center"
+                >
+                  <img
+                    :alt="slotProps.option.label"
+                    :src="generateFlagImageUrl(slotProps.option.code)"
+                    style="width: 18px ;background-color: #333;"
+                  />
+                  <div>{{ slotProps.option.label }}</div>
+                </div>
+              </template>
+            </Dropdown>
+          </div>
+        </div>
+        <div class="card">
+          <Card style="width: 25em">
+            <template #header>
+              <img
+                style="width: 100%"
+                alt="user header"
+                src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/200397129/original/6a2aebde0a11fed47eaaad653c7bbd4485d214bb/setup-and-manage-google-ads-adwords-ppc-campaign.png"
+              />
+            </template>
+            <template #title> Advanced Card </template>
+            <template #subtitle> Card subtitle </template>
+            <template #content>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Inventore sed consequuntur error repudiandae numquam deserunt
+                quisquam repellat libero asperiores earum nam nobis, culpa
+                ratione quam perferendis esse, cupiditate neque quas!
+              </p>
+            </template>
+            <template #footer>
+              <Button icon="pi pi-check" label="Save" />
+              <Button
+                label="Cancel"
+                severity="secondary"
+                style="margin-left: 0.5rem"
+              ></Button>
+            </template>
+          </Card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-a#pv_id_1_0_header_action {
-  background-color: #6366f1;
+.card {
+  width: auto;
 }
-/* .accordion div:nth-child(3) a#pv_id_1_0_header_action{
-  background: #6366f1 ;
-} */
+.splitter {
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+}
+.imgAdsContainer {
+  width: 90%;
+  display: flex;
+  justify-content: space-around;
+}
+.imgAdsSection {
+  display: flex;
+  margin-top: 5rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
 .informationSection {
   display: flex;
   margin-top: 5rem;
